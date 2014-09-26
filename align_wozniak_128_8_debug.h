f13e4c7ab5dadd01a352cf0ec0fbf9d9e881ac32
Jeff Daily <jeff.daily@pnnl.gov>
2014-09-26 16:14:37 -0700

    impl 128 8bit wozniak nw and sg

--- align_wozniak_128_8_debug.h
+++ align_wozniak_128_8_debug.h
@@ -10,56 +10,67 @@ extern "C" {
 
 #define MAX(a,b) ((a)>(b)?(a):(b))
 
-/*#define NEG_INF_8 (INT8_MIN/(int8_t)2)*/
 #define NEG_INF_8 INT8_MIN
 
-extern int nw_wozniak_128_8(
+extern int nw_wozniak_128_8_debug(
         const char * const restrict _s1, const int s1Len,
         const char * const restrict _s2, const int s2Len,
         const int open, const int gap,
         const int matrix[24][24],
-        int * const restrict tbl_pr, int * const restrict del_pr);
+        int * const restrict tbl_pr, int * const restrict del_pr,
+        int * const restrict score_table);
 
-extern int sg_wozniak_128_8(
+extern int sg_wozniak_128_8_debug(
         const char * const restrict _s1, const int s1Len,
         const char * const restrict _s2, const int s2Len,
         const int open, const int gap,
         const int matrix[24][24],
-        int * const restrict tbl_pr, int * const restrict del_pr);
+        int * const restrict tbl_pr, int * const restrict del_pr,
+        int * const restrict score_table);
 
-extern int sw_wozniak_128_8(
+extern int sw_wozniak_128_8_debug(
         const char * const restrict _s1, const int s1Len,
         const char * const restrict _s2, const int s2Len,
         const int open, const int gap,
         const int matrix[24][24],
-        int * const restrict tbl_pr, int * const restrict del_pr);
+        int * const restrict tbl_pr, int * const restrict del_pr,
+        int * const restrict score_table);
 
-extern int nw_stats_wozniak_128_8(
+extern int nw_stats_wozniak_128_8_debug(
         const char * const restrict _s1, const int s1Len,
         const char * const restrict _s2, const int s2Len,
         const int open, const int gap,
         const int matrix[24][24],
         int * const restrict matches, int * const restrict length,
         int * const restrict tbl_pr, int * const restrict del_pr,
-        int * const restrict mch_pr, int * const restrict len_pr);
+        int * const restrict mch_pr, int * const restrict len_pr,
+        int * const restrict score_table,
+        int * const restrict match_table,
+        int * const restrict length_table);
 
-extern int sg_stats_wozniak_128_8(
+extern int sg_stats_wozniak_128_8_debug(
         const char * const restrict _s1, const int s1Len,
         const char * const restrict _s2, const int s2Len,
         const int open, const int gap,
         const int matrix[24][24],
         int * const restrict matches, int * const restrict length,
         int * const restrict tbl_pr, int * const restrict del_pr,
-        int * const restrict mch_pr, int * const restrict len_pr);
+        int * const restrict mch_pr, int * const restrict len_pr,
+        int * const restrict score_table,
+        int * const restrict match_table,
+        int * const restrict length_table);
 
-extern int sw_stats_wozniak_128_8(
+extern int sw_stats_wozniak_128_8_debug(
         const char * const restrict _s1, const int s1Len,
         const char * const restrict _s2, const int s2Len,
         const int open, const int gap,
         const int matrix[24][24],
         int * const restrict matches, int * const restrict length,
         int * const restrict tbl_pr, int * const restrict del_pr,
-        int * const restrict mch_pr, int * const restrict len_pr);
+        int * const restrict mch_pr, int * const restrict len_pr,
+        int * const restrict score_table,
+        int * const restrict match_table,
+        int * const restrict length_table);
 
 #ifdef __cplusplus
 }

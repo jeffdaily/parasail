d011af0a65a1a6f9ffe7b59ccb75d10a5568a058
Jeff Daily <jeff.daily@pnnl.gov>
2014-10-15 20:38:31 -0700

    add sg_scan_col

---
+++ sg_scan_col.c
@@ -0,0 +1,115 @@
+#include "config.h"
+
+#include <stdlib.h>
+
+#ifdef ALIGN_EXTRA
+#include "align/align_debug.h"
+#else
+#include "align/align.h"
+#endif
+#include "blosum/blosum_map.h"
+
+#ifdef ALIGN_EXTRA
+#define FNAME sg_scan_col_debug
+#else
+#define FNAME sg_scan_col
+#endif
+
+int FNAME(
+        const char * const restrict _s1, const int s1Len,
+        const char * const restrict _s2, const int s2Len,
+        const int open, const int gap,
+        const int matrix[24][24],
+        int * const restrict _H, int * const restrict E
+#ifdef ALIGN_EXTRA
+        , int * const restrict score_table
+#endif
+        )
+{
+    int * const restrict s1 = (int * const restrict)malloc(sizeof(int)*s1Len);
+    int * const restrict s2 = (int * const restrict)malloc(sizeof(int)*s2Len);
+    int * const restrict HtB= (int * const restrict)malloc(sizeof(int)*(s1Len+1));
+    int * const restrict Ht = HtB+1;
+    int * const restrict FtB= (int * const restrict)malloc(sizeof(int)*(s1Len+1));
+    int * const restrict Ft = FtB+1;
+    int i = 0;
+    int j = 0;
+    int * const restrict H = _H+1;
+    int score = NEG_INF_32;
+
+    for (i=0; i<s1Len; ++i) {
+        s1[i] = MAP_BLOSUM_[(unsigned char)_s1[i]];
+    }
+    for (j=0; j<s2Len; ++j) {
+        s2[j] = MAP_BLOSUM_[(unsigned char)_s2[j]];
+    }
+
+    /* initialize H */
+    H[-1] = 0;
+    Ht[-1] = 0;
+    for (i=0; i<s1Len; ++i) {
+        H[i] = 0;
+    }
+    Ft[-1] = NEG_INF_32;
+
+    /* initialize E */
+    for (i=0; i<s1Len; ++i) {
+        E[i] = NEG_INF_32;
+    }
+
+    /* iterate over database */
+    for (j=0; j<s2Len-1; ++j) {
+        const int * const restrict matcol = matrix[s2[j]];
+        /* calculate E */
+        for (i=0; i<s1Len; ++i) {
+            E[i] = MAX(E[i]-gap, H[i]-open);
+        }
+        /* calculate Ht */
+        for (i=0; i<s1Len; ++i) {
+            Ht[i] = MAX(H[i-1]+matcol[s1[i]], E[i]);
+        }
+        /* calculate Ft */
+        for (i=0; i<s1Len; ++i) {
+            Ft[i] = MAX(Ft[i-1]-gap, Ht[i-1]);
+        }
+        /* calculate H */
+        for (i=0; i<s1Len; ++i) {
+            H[i] = MAX(Ht[i], Ft[i]-open);
+#ifdef ALIGN_EXTRA
+            score_table[i*s2Len + j] = H[i];
+#endif
+        }
+        score = MAX(score, H[s1Len-1]);
+    }
+    {
+        j = s2Len - 1;
+        const int * const restrict matcol = matrix[s2[j]];
+        /* calculate E */
+        for (i=0; i<s1Len; ++i) {
+            E[i] = MAX(E[i]-gap, H[i]-open);
+        }
+        /* calculate Ht */
+        for (i=0; i<s1Len; ++i) {
+            Ht[i] = MAX(H[i-1]+matcol[s1[i]], E[i]);
+        }
+        /* calculate Ft */
+        for (i=0; i<s1Len; ++i) {
+            Ft[i] = MAX(Ft[i-1]-gap, Ht[i-1]);
+        }
+        /* calculate H */
+        for (i=0; i<s1Len; ++i) {
+            H[i] = MAX(Ht[i], Ft[i]-open);
+#ifdef ALIGN_EXTRA
+            score_table[i*s2Len + j] = H[i];
+#endif
+            score = MAX(score, H[s1Len-1]);
+        }
+    }
+
+    free(s1);
+    free(s2);
+    free(HtB);
+    free(FtB);
+
+    return score;
+}

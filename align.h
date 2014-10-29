c966292d79225e820843785b43a06fc62f728dde
Jeff Daily <jeff.daily@pnnl.gov>
2014-10-28 22:44:12 -0700

    missing sg_stats_scan sw_stats_scan signatures

--- align.h
+++ align.h
@@ -88,7 +88,25 @@ extern int sg_stats(
         int * const restrict tbl_pr, int * const restrict del_pr,
         int * const restrict mch_pr, int * const restrict len_pr);
 
+extern int sg_stats_scan(
+        const char * const restrict _s1, const int s1Len,
+        const char * const restrict _s2, const int s2Len,
+        const int open, const int gap,
+        const int matrix[24][24],
+        int * const restrict matches, int * const restrict length,
+        int * const restrict tbl_pr, int * const restrict del_pr,
+        int * const restrict mch_pr, int * const restrict len_pr);
+
 extern int sw_stats(
+        const char * const restrict _s1, const int s1Len,
+        const char * const restrict _s2, const int s2Len,
+        const int open, const int gap,
+        const int matrix[24][24],
+        int * const restrict matches, int * const restrict length,
+        int * const restrict tbl_pr, int * const restrict del_pr,
+        int * const restrict mch_pr, int * const restrict len_pr);
+
+extern int sw_stats_scan(
         const char * const restrict _s1, const int s1Len,
         const char * const restrict _s2, const int s2Len,
         const int open, const int gap,

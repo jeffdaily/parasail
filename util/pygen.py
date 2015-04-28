2af68e6d5b2c9567915ff929391c39ee692ccde6
Jeff Daily <jeff.daily@pnnl.gov>
2015-04-28 11:58:16 -0700

    first attempt at python bindings

---
+++ util/pygen.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+# Creates the parasail.pyx file used for the python bindings.
+
+import glob
+import os
+
+print """from libc.string cimport const_char
+
+cdef extern from "parasail.h":
+    ctypedef struct parasail_result_t:
+        int saturated
+        int score
+        int matches
+        int similar
+        int length
+        #int * restrict score_table
+        #int * restrict matches_table
+        #int * restrict similar_table
+        #int * restrict length_table
+
+    ctypedef struct parasail_matrix_t:
+        pass
+
+    void parasail_result_free(parasail_result_t * result)
+"""
+
+# serial reference implementations (3x2x2x2 = 24 impl)
+alg = ["nw", "sg", "sw"]
+stats = ["", "_stats"]
+table = ["", "_table"]
+for a in alg:
+    for s in stats:
+        for t in table:
+            print ""
+            print " "*4+"parasail_result_t* parasail_"+a+s+t+'('
+            print " "*8+"const_char * s1, const int s1Len,"
+            print " "*8+"const_char * s2, const int s2Len,"
+            print " "*8+"const int open, const int gap,"
+            print " "*8+"const parasail_matrix_t* matrix);"
+
+# serial scan reference implementations (3x2x2x2 = 24 impl)
+alg = ["nw", "sg", "sw"]
+stats = ["", "_stats"]
+table = ["", "_table"]
+for a in alg:
+    for s in stats:
+        for t in table:
+            print ""
+            print " "*4+"parasail_result_t* parasail_"+a+s+t+'_scan('
+            print " "*8+"const_char * s1, const int s1Len,"
+            print " "*8+"const_char * s2, const int s2Len,"
+            print " "*8+"const int open, const int gap,"
+            print " "*8+"const parasail_matrix_t* matrix);"
+
+# vectorized implementations (3x2x2x3x13 = 468 impl)
+alg = ["nw", "sg", "sw"]
+stats = ["", "_stats"]
+table = ["", "_table"]
+par = ["_scan", "_striped", "_diag"]
+width = ["_64","_32","_16","_8"]
+
+for a in alg:
+    for s in stats:
+        for t in table:
+            for p in par:
+                for w in width:
+                    print ""
+                    print " "*4+"parasail_result_t* parasail_"+a+s+t+p+w+'('
+                    print " "*8+"const_char * s1, const int s1Len,"
+                    print " "*8+"const_char * s2, const int s2Len,"
+                    print " "*8+"const int open, const int gap,"
+                    print " "*8+"const parasail_matrix_t* matrix);"
+
+# vectorized implementations of blocked (just a couple)
+alg = ["sw"]
+stats = [""]
+table = ["", "_table"]
+par = ["_blocked"]
+width = ["_32","_16"]
+
+for a in alg:
+    for s in stats:
+        for t in table:
+            for p in par:
+                for w in width:
+                    print ""
+                    print " "*4+"parasail_result_t* parasail_"+a+s+t+p+w+'('
+                    print " "*8+"const_char * s1, const int s1Len,"
+                    print " "*8+"const_char * s2, const int s2Len,"
+                    print " "*8+"const int open, const int gap,"
+                    print " "*8+"const parasail_matrix_t* matrix);"
+
+
+# now for the actual implementations and not just the header info
+
+print """
+cdef class Result:
+    cdef parasail_result_t *_c_object
+    def __dealloc__(self):
+        if self._c_object is not NULL:
+            parasail_result_free(self._c_object)
+    @staticmethod
+    cdef create(parasail_result_t *c_object):
+        p = Result()
+        p._c_object = c_object
+        return p
+    property saturated:
+        def __get__(self): return self._c_object.saturated
+    property score:
+        def __get__(self): return self._c_object.score
+    property matches:
+        def __get__(self): return self._c_object.matches
+    property similar:
+        def __get__(self): return self._c_object.similar
+    property length:
+        def __get__(self): return self._c_object.length
+    def __str__(self):
+        return str(self.score)
+    def __int__(self):
+        return self.score
+    def __long__(self):
+        return self.score
+"""
+print """
+cdef class Matrix:
+    cdef const parasail_matrix_t *_c_object
+    @staticmethod
+    cdef create(const parasail_matrix_t *c_object):
+        p = Matrix()
+        p._c_object = c_object
+        return p
+"""
+
+prefix = "matrices/"
+filenames = []
+filenames.extend(glob.glob(prefix+"BLOSUM*"))
+filenames.extend(glob.glob(prefix+"PAM*"))
+names = [f[len(prefix):].lower() for f in filenames]
+
+for name in names:
+    print 'cdef extern from "parasail/matrices/%s.h":' % name
+    print ' '*4+"const parasail_matrix_t parasail_"+name
+for name in names:
+    print "%s = Matrix.create(&parasail_%s)" % (name,name)
+
+# serial reference implementations (3x2x2x2 = 24 impl)
+alg = ["nw", "sg", "sw"]
+stats = ["", "_stats"]
+table = ["", "_table"]
+for a in alg:
+    for s in stats:
+        for t in table:
+            print ""
+            print "def "+a+s+t+'('
+            print " "*8+"const_char * s1,"
+            print " "*8+"const_char * s2,"
+            print " "*8+"const int open, const int gap,"
+            print " "*8+"Matrix matrix not None):"
+            print " "*4+"return Result.create(parasail_"+a+s+t+"(s1,len(s1),s2,len(s2),open,gap,matrix._c_object))"
+

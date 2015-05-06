fb6d5516167c20a5cc540803cd6effd4d3ae5f98
Jeff Daily <jeff.daily@pnnl.gov>
2015-05-05 19:09:59 -0700

    add configure test for _aligned_malloc

--- src/memory.c
+++ src/memory.c
@@ -24,7 +24,9 @@
 void* parasail_memalign(size_t alignment, size_t size)
 {
     void *ptr = NULL;
-#if defined(HAVE_POSIX_MEMALIGN)
+#if defined(HAVE__ALIGNED_MALLOC)
+    ptr = _aligned_malloc(size, alignment);
+#elif defined(HAVE_POSIX_MEMALIGN)
     int retcode = posix_memalign(&ptr, alignment, size);
     assert(0 == retcode);
 #elif defined(HAVE_ALIGNED_ALLOC)
@@ -40,7 +42,11 @@ void* parasail_memalign(size_t alignment, size_t size)
 
 void parasail_free(void *ptr)
 {
+#if defined(HAVE__ALIGNED_MALLOC)
+     _aligned_free(ptr);
+#else
     free(ptr);
+#endif
 }
 
 int * parasail_memalign_int(size_t alignment, size_t size)

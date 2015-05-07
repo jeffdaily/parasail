7ba20228d3dc8298c865f5076a1e7a904998ada0
Jeff Daily <jeff.daily@pnnl.gov>
2015-05-06 20:53:30 -0700

    a few more print statements in the python test

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -4,3 +4,14 @@ result = parasail.sw("asdf","asdf",10,1,parasail.blosum62)
 print result
 print result.saturated
 print result.score
+print result.matches
+print result.similar
+print result.length
+
+result = parasail.sw_stats("asdf","asdf",10,1,parasail.blosum62)
+print result
+print result.saturated
+print result.score
+print result.matches
+print result.similar
+print result.length

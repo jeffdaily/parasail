d413936b95b7d743911c8b93a5918b1649d75b1e
Jeff Daily <jeff.daily@pnnl.gov>
2016-02-26 13:58:30 -0800

    fix mixed declarations and code in kseq.h

--- contrib/kseq.h
+++ contrib/kseq.h
@@ -39,8 +39,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define KS_SEP_SPACE 0 // isspace(): \t, \n, \v, \f, \r
-#define KS_SEP_TAB   1 // isspace() && !' '
+#define KS_SEP_SPACE 0 /* isspace(): \t, \n, \v, \f, \r */
+#define KS_SEP_TAB   1 /* isspace() && !' ' */
 #define KS_SEP_MAX   1
 
 #define __KS_TYPE(type_t)						\

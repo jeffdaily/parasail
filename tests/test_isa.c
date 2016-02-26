d9e660d8a4b7f359bcb653d640043647d53eb7a4
Jeff Daily <jeff.daily@pnnl.gov>
2016-02-26 14:46:55 -0800

    Fixes #15.

--- tests/test_isa.c
+++ tests/test_isa.c
@@ -6,37 +6,62 @@
 
 #define UNUSED(expr) do { (void)(expr); } while (0)
 
+static inline char* yesno(int value)
+{
+    return value ? "yes" : "no ";
+}
+
+
 int main(int argc, char **argv)
 {
+    int cpu_sse2;
+    int cpu_sse41;
+    int cpu_avx2;
+    int cpu_knc;
+    int cc_sse2;
+    int cc_sse41;
+    int cc_avx2;
+    int cc_knc;
+
     UNUSED(argc);
     UNUSED(argv);
 
-    if (parasail_can_use_sse2()) {
-        printf("sse2 supported\n");
-    }
-    else {
-        printf("sse2 NOT supported\n");
-    }
+    cpu_sse2 = parasail_can_use_sse2();
+    cpu_sse41 = parasail_can_use_sse41();
+    cpu_avx2 = parasail_can_use_avx2();
 
-    if (parasail_can_use_sse41()) {
-        printf("sse41 supported\n");
-    }
-    else {
-        printf("sse41 NOT supported\n");
-    }
+#if HAVE_SSE2
+    cc_sse2 = 1;
+#else
+    cc_sse2 = 0;
+#endif
 
-    if (parasail_can_use_avx2()) {
-        printf("avx2 supported\n");
-    }
-    else {
-        printf("avx2 NOT supported\n");
-    }
+#if HAVE_SSE41
+    cc_sse41 = 1;
+#else
+    cc_sse41 = 0;
+#endif
+
+#if HAVE_AVX2
+    cc_avx2 = 1;
+#else
+    cc_avx2 = 0;
+#endif
 
 #ifdef __MIC__
-    printf("knc supported\n");
+    cpu_knc = 1;
+    cc_knc = 1;
 #else
-    printf("knc NOT supported\n");
+    cpu_knc = 0;
+    cc_knc = 0;
 #endif
+
+    printf(" ISA   | Compiler | CPU  \n");
+    printf("-------------------------\n");
+    printf(" SSE2  |  %s     | %s\n", yesno(cc_sse2), yesno(cpu_sse2));
+    printf(" SSE41 |  %s     | %s\n", yesno(cc_sse41), yesno(cpu_sse41));
+    printf(" AVX2  |  %s     | %s\n", yesno(cc_avx2), yesno(cpu_avx2));
+    printf(" KNC   |  %s     | %s\n", yesno(cc_knc), yesno(cpu_knc));
 
     return 0;
 }

bfdecf954529ebeb694e12b66f2a892f100f2174
Jeff Daily <jeff.daily@pnnl.gov>
2017-09-19 12:00:38 -0700

    initial ssw emulation implemented

---
+++ src/pssw.c
@@ -0,0 +1,148 @@
+/**
+ * @file
+ *
+ * @author jeff.daily@pnnl.gov
+ *
+ * Copyright (c) 2015 Battelle Memorial Institute.
+ */
+#include "config.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "parasail.h"
+#include "parasail/memory.h"
+
+parasail_profile_t* parasail_ssw_init(
+        const char * const restrict s1, const int s1Len,
+        const parasail_matrix_t* matrix, const int8_t score_size)
+{
+    parasail_profile_t *profile8 = NULL;
+    parasail_profile_t *profile16 = NULL;
+
+    if (score_size == 0 || score_size == 2) {
+        profile8 = parasail_profile_create_8(s1, s1Len, matrix);
+    }
+    if (score_size == 1 || score_size == 2) {
+        profile16 = parasail_profile_create_16(s1, s1Len, matrix);
+    }
+
+    if (profile8 && profile16) {
+        profile8->profile16 = profile16->profile16;
+        free(profile16);
+        return profile8;
+    }
+    else if (profile8) {
+        return profile8;
+    }
+    else if (profile16) {
+        return profile16;
+    }
+
+    return NULL;
+}
+
+parasail_result_ssw_t* parasail_ssw(
+        const char * const restrict s1, const int s1Len,
+        const char * const restrict s2, const int s2Len,
+        const int open, const int gap, const parasail_matrix_t* matrix)
+{
+    parasail_profile_t *profile = parasail_ssw_init(s1, s1Len, matrix, 2);
+    parasail_result_ssw_t *result = parasail_ssw_profile(profile, s2, s2Len, open, gap);
+    parasail_profile_free(profile);
+    return result;
+}
+
+parasail_result_ssw_t* parasail_ssw_profile(
+        const parasail_profile_t * const restrict profile,
+        const char * const restrict s2, const int s2Len,
+        const int open, const int gap)
+{
+    const char *s1 = profile->s1;
+    const parasail_matrix_t *matrix = profile->matrix;
+    parasail_result_t *result = NULL;
+    parasail_result_ssw_t *result_ssw = NULL;
+    parasail_cigar_t *cigar = NULL;
+    int word = 0;
+
+    /* find the end loc first with the faster implementation */
+    result = parasail_sw_striped_profile_8(profile, s2, s2Len, open, gap);
+    if (parasail_result_is_saturated(result)) {
+        word = 1;
+        parasail_result_free(result);
+        result = parasail_sw_striped_profile_16(profile, s2, s2Len, open, gap);
+    }
+
+    if (!parasail_result_is_saturated(result)) {
+        parasail_result_t *result_reverse = NULL;
+        parasail_result_t *result_final = NULL;
+        char *s1_reverse = NULL;
+        char *s2_reverse = NULL;
+        int s1Len = 0;
+        int s2Len = 0;
+        int s1Off = 0;
+        int s2Off = 0;
+
+        /* find beginning loc by going in reverse */
+        s1_reverse = parasail_reverse(s1, result->end_query);
+        s2_reverse = parasail_reverse(s2, result->end_ref);
+        s1Len = result->end_query+1;
+        s2Len = result->end_ref+1;
+        if (0 == word) {
+            result_reverse = parasail_sw_striped_8(
+                    s1_reverse, s1Len,
+                    s2_reverse, s2Len,
+                    open, gap, matrix);
+        }
+        else {
+            result_reverse = parasail_sw_striped_16(
+                    s1_reverse, s1Len,
+                    s2_reverse, s2Len,
+                    open, gap, matrix);
+        }
+        free(s2_reverse);
+        free(s1_reverse);
+
+        /* run trace version of sw on just the aligned portion */
+        s1Off = result->end_query - result_reverse->end_query;
+        s2Off = result->end_ref - result_reverse->end_ref;
+        s1Len = result_reverse->end_query+1;
+        s2Len = result_reverse->end_ref+1;
+        if (0 == word) {
+            result_final = parasail_sw_trace_striped_8(
+                    &s1[s1Off], s1Len,
+                    &s2[s2Off], s2Len,
+                    open, gap, matrix);
+        }
+        else {
+            result_final = parasail_sw_trace_striped_16(
+                    &s1[s1Off], s1Len,
+                    &s2[s2Off], s2Len,
+                    open, gap, matrix);
+        }
+
+        /* get cigar from traceback */
+        cigar = parasail_result_get_cigar(result_final,
+                &s1[s1Off], s1Len,
+                &s2[s2Off], s2Len,
+                matrix);
+
+        result_ssw = (parasail_result_ssw_t*)malloc(sizeof(parasail_result_ssw_t));
+        result_ssw->score1 = result_final->score;
+        result_ssw->ref_begin1 = s1Off;
+        result_ssw->ref_end1 = s1Len;
+        result_ssw->read_begin1 = s2Off;
+        result_ssw->read_end1 = s2Len;
+        result_ssw->cigar = cigar->seq;
+        result_ssw->cigarLen = cigar->len;
+    }
+
+    return result_ssw;;
+}
+
+void parasail_result_ssw_free(parasail_result_ssw_t *result)
+{
+    free(result->cigar);
+    free(result);
+}
+

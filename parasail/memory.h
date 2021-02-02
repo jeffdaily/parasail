e04806968222feb79563a5adc2946f1db67494f4
Jeff Daily <jeffrey.daily@gmail.com>
2021-02-02 00:43:09 +0000

    fix missing __func__ in MSVC

--- parasail/memory.h
+++ parasail/memory.h
@@ -12,6 +12,14 @@
 
 #include "parasail.h"
 
+#if __STDC_VERSION__ < 199901L
+# if __GNUC__ >= 2 || _MSC_VER >= 1300
+#  define __func__ __FUNCTION__
+# else
+#  define __func__ "<unknown>"
+# endif
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
